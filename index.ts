b73cf32d7ac1224cb83f05bc50c492c2dc42112d
DaneVN <Dane.vN23@gmail.com>
2024-11-19 21:10:57 +0200

    Primitive - mix type assigned

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+import { join } from "lit-html/directives/join.js";
+
+const returningUserDisplay = document.querySelector(
+  "#returning-user"
+) as HTMLSpanElement;
+const userNameDisplay = document.querySelector("#user") as HTMLSpanElement;
 const reviewTotalDisplay = document.querySelector(
   "#reviews"
 ) as HTMLHeadingElement;
@@ -23,11 +29,12 @@ const reviews = [
   },
 ];
 
-// Boolean Types mini-challenge
-// if the last reviewer is a loyalty User, can you add a star to the end of their name?
-// please do so in the existing function, and make sure to declare what type of
-// parameters the function takes.
-// : boolean
+// Fixing the Website
+
+// Can you fix my code to show 'Welcome back Bobby' in the Nav Bar for
+// our couch surfing website? Do this by assigning types to the parameters
+// we pass to our populateUser function, so that we can be aware of
+// errors in our user objects in the future.
 
 function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
   const iconDisplay = isLoyalty ? "⭐" : "";
@@ -41,3 +48,38 @@ function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+const you = {
+  userName: { firstName: "Bobby", lastName: "Brown" },
+  isReturning: "yes",
+};
+
+function populateUser(
+  isReturning: boolean,
+  userName: { firstName: string; lastName: string }
+) {
+  if (isReturning) {
+    returningUserDisplay.innerHTML = "back";
+  }
+  userNameDisplay.innerHTML = Object.values(userName).join(" ");
+}
+
+populateUser(Boolean(you.isReturning), you.userName);
+
+//solution:
+// const you = {
+//     userName: { firstName: "Bobby", lastName: "Brown" },
+//     isReturning: "yes",
+//   };
+
+//   function populateUser(
+//     isReturning: boolean,
+//     userName: { firstName: string; lastName: string }
+//   ) {
+//     if (isReturning) {
+//       returningUserDisplay.innerHTML = "back";
+//     }
+//     userNameDisplay.innerHTML = Object.values(userName).join(" ");
+//   }
+
+//   populateUser(Boolean(you.isReturning), you.userName);
