ceb941f315ad828341121dfe5a7e950d6be015b1
DaneVN <Dane.vN23@gmail.com>
2024-11-20 05:22:22 +0200

    Structural - Object type

--- index.ts
+++ index.ts
@@ -3,10 +3,14 @@ import { join } from "lit-html/directives/join.js";
 const returningUserDisplay = document.querySelector(
   "#returning-user"
 ) as HTMLSpanElement;
-const userNameDisplay = document.querySelector("#user") as HTMLSpanElement;
+const firstNameDisplay = document.querySelector("#user") as HTMLSpanElement;
 const reviewTotalDisplay = document.querySelector(
   "#reviews"
 ) as HTMLHeadingElement;
+
+//redundant to assign `age: number`, rather assign type to valiables that
+// don't have a value yet, like parameters or `let age;`
+const age = 23;
 
 const reviews = [
   {
@@ -29,13 +33,6 @@ const reviews = [
   },
 ];
 
-// Fixing the Website
-
-// Can you fix my code to show 'Welcome back Bobby' in the Nav Bar for
-// our couch surfing website? Do this by assigning types to the parameters
-// we pass to our populateUser function, so that we can be aware of
-// errors in our user objects in the future.
-
 function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
   const iconDisplay = isLoyalty ? "⭐" : "";
   reviewTotalDisplay.innerHTML =
@@ -49,37 +46,29 @@ function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 
-const you = {
-  userName: { firstName: "Bobby", lastName: "Brown" },
-  isReturning: "yes",
+// Object Types
+// In this scrim, based on what we just learnt, can you change userName to firstName, then
+// add an age and lastName to the 'you' Object, making sure to account for it in the Specialised
+// Object for defining its structure.
+
+//`: {}` = `: object`
+const you: {
+  firstName: string;
+  lastName: string;
+  age: number;
+  isReturning: boolean;
+} = {
+  firstName: "Bobby",
+  lastName: "Bobberson",
+  age: 40,
+  isReturning: true,
 };
 
-function populateUser(
-  isReturning: boolean,
-  userName: { firstName: string; lastName: string }
-) {
+function populateUser(isReturning: boolean, firstName: string) {
   if (isReturning) {
     returningUserDisplay.innerHTML = "back";
   }
-  userNameDisplay.innerHTML = Object.values(userName).join(" ");
+  firstNameDisplay.innerHTML = firstName;
 }
 
-populateUser(Boolean(you.isReturning), you.userName);
-
-//solution:
-// const you = {
-//     userName: { firstName: "Bobby", lastName: "Brown" },
-//     isReturning: "yes",
-//   };
-
-//   function populateUser(
-//     isReturning: boolean,
-//     userName: { firstName: string; lastName: string }
-//   ) {
-//     if (isReturning) {
-//       returningUserDisplay.innerHTML = "back";
-//     }
-//     userNameDisplay.innerHTML = Object.values(userName).join(" ");
-//   }
-
-//   populateUser(Boolean(you.isReturning), you.userName);
+populateUser(you.isReturning, you.firstName);
